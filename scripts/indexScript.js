29b9092a04b11ab76b18ae3d21de8c01156f8b59
AlcoMiJoe <kaiyu.la.paz@gmail.com>
2024-11-27 01:45:16 +0800

    01:45 27/11/24 3rd Commit
    Changes Made:
    
    Reworked Navigation Bar
        - Responsive
        - Sidebar Menu added for narrower screens
        - Spacing and Code reworked to be cleaner/more consistent
    
    Cleaned up CSS files
    
    Added JS files under 'scripts/'
        -
    New paragraph + picture template combo created
    
    Additions/Changes to be Made in order of priority:
    
    Responsive Headers/Footer
    Resizing image section in index.html to be fixed
    Merchandise Page
    About Page
        -Background Images
        -Profile Images
    Carousel for albums in #music in index.html

---
+++ scripts/indexScript.js
@@ -0,0 +1,9 @@
+function showNavSidebar(){
+    const sidebar = document.querySelector('.navSidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideNavSidebar(){
+    const sidebar = document.querySelector('.navSidebar')
+    sidebar.style.display = 'none'
+}
